b74601d8583e60634717632f64096c48bfb35913
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2022-10-31 12:00:16 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+// configuração do servidor
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+//renderização font-end
+app.use(express.json())
+app.set('view engine', 'ejs')
+app.set('view engine', 'html')
+//exportar os dados de conexão do servidor
+module.exports = {app,port}
---
+++ src/app.js
@@ -0,0 +1,10 @@
+//importando as configurações do servidor
+const {app,port} = require('../server')
+
+//rotas
+const index = require('./routes/index')(app)
+const sobre = require('./routes/sobre')(app)
+//escuta do servidor
+app.listen(port, ()=>{
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
